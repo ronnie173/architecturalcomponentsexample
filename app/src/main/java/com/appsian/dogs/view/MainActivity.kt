835779d296b5bb0a70fb3fefa684be4fc9b6434b
Jerome Raymond <ronnie173@gmail.com>
2019-08-23 22:05:22 -0500

    initial commit

---
+++ app/src/main/java/com/appsian/dogs/view/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.appsian.dogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.appsian.dogs.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController:NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
+    }
+}
